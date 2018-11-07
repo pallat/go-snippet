4c91f209aea36e1c9cbc786480e89624ca19148a
Pallat Anchaleechamaikorn <pallat@HQ8636610595N1.local>
2018-11-07 18:00:19 +0700

    change type from gin.H to map[string]string

--- api/feature.go
+++ api/feature.go
@@ -1,12 +1,11 @@
 package api
 
 import (
-	"github.com/gin-gonic/gin"
 	"ktb.co.th/api/prototype/pkg/handler"
 )
 
 func Feature(c *handler.Context) {
-	c.JSON(200, gin.H{
+	c.JSON(200, map[string]string{
 		"message": "pong",
 	})
 }
