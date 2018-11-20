6e83fde26f91e56470f4eaa963f8c1d92fe08f60
Pallat Anchaleechamaikorn <pallat.anchaleechamaikorn@ktb.co.th>
2018-11-20 10:12:44 +0700

    experiment echo with log

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,52 @@
 package main
 
 import (
+	"io"
+	"net/http"
 	"os"
 
-	"github.com/gin-gonic/gin"
-	"ktb.co.th/api/prototype/api"
-	"ktb.co.th/api/prototype/pkg/handler"
+	"github.com/labstack/echo"
+	"github.com/labstack/echo/middleware"
 	"ktb.co.th/api/prototype/pkg/logs"
 )
 
+// Handler
+func hello(c echo.Context) error {
+	c.Logger().Info(c.Response().Header().Get(echo.HeaderXRequestID))
+
+	return c.String(http.StatusOK, "Hello, World!")
+}
+
 func main() {
-	r := gin.Default()
-	// gin.DebugPrintRouteFunc = GinPrintLog
-	l := logs.New(os.Stdout)
-	r.POST("/getfatcas", handler.Serve(api.Fatca, l))
-	r.Run() // listen and serve on 0.0.0.0:8080
+	var logout io.Writer = os.Stdout
+
+	// Echo instance
+	e := echo.New()
+
+	// Middleware
+	e.Use(middleware.LoggerWithConfig(
+		middleware.LoggerConfig{
+			Skipper: middleware.DefaultSkipper,
+			Format: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}","host":"${host}",` +
+				`"method":"${method}","uri":"${uri}","status":${status}, "latency":${latency},` +
+				`"latency_human":"${latency_human}","bytes_in":${bytes_in},` +
+				`"bytes_out":${bytes_out}}` + "\n",
+			Output: logout,
+		},
+	))
+	e.Use(middleware.Recover())
+	e.Use(middleware.RequestID())
+
+	e.Use(middleware.BodyDump(func(c echo.Context, reqBody, resBody []byte) {
+		c.Logger().SetOutput(logout)
+		c.Logger().Info(c.Response().Header().Get(echo.HeaderXRequestID) + "," + string(reqBody))
+		c.Logger().Info(c.Response().Header().Get(echo.HeaderXRequestID) + "," + string(resBody))
+	}))
+
+	e.Use(logs.MiddlewareWriter(logout))
+
+	// Routes
+	e.GET("/", hello)
+
+	e.Logger.Fatal(e.Start(":1323"))
 }
--- pkg/logs/log.go
+++ pkg/logs/log.go
@@ -1,32 +1,18 @@
 package logs
 
 import (
-	"fmt"
 	"io"
+
+	"github.com/labstack/echo"
+	"github.com/labstack/gommon/log"
 )
 
-type Level int
-
-const (
-	PanicLevel Level = iota
-	FatalLevel
-	ErrorLevel
-	WarnLevel
-	InfoLevel
-	DebugLevel
-	TraceLevel
-)
-
-func New(w io.Writer) Logger {
-	return Logger{w: w}
+func MiddlewareWriter(w io.Writer) echo.MiddlewareFunc {
+	return func(handler echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			c.Logger().SetOutput(w)
+			c.Logger().SetLevel(log.INFO)
+			return handler(c)
+		}
+	}
 }
-
-type Logger struct {
-	w io.Writer
-}
-
-func (Logger) Info(args ...interface{}) {}
-func (l Logger) Debug(args ...interface{}) {
-	fmt.Fprintln(l.w, args...)
-}
-func (Logger) Error(args ...interface{}) {}
